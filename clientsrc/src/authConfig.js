969f1d196b60624606872d128d15289cf1f792ca
CoreyWhitmore <cjwhit97@hotmail.com>
2020-09-21 14:07:12 -0600

    Initial Commit

---
+++ clientsrc/src/authConfig.js
@@ -0,0 +1,3 @@
+export const domain = "corey101997.us.auth0.com";
+export const audience = "https://kanban";
+export const clientId = "gzcQjg26qEZyhmS9POrTS00tSPHct1YJ";
---
+++ server/controllers/CommentsController.js
@@ -0,0 +1,50 @@
+import express from 'express'
+import BaseController from "../utils/BaseController";
+import auth0provider from "@bcwdev/auth0provider";
+import { commentService } from '../services/CommentService'
+
+
+
+//PUBLIC
+export class CommentsController extends BaseController {
+    constructor() {
+        super("api/comments")
+        this.router
+            .use(auth0provider.getAuthorizedUserInfo)
+            .get('/:id', this.getById)
+            .post('', this.create)
+            .put('/:id', this.edit)
+            .delete('/:id', this.delete)
+    }
+
+    async getById(req, res, next) {
+        try {
+            let data = await commentService.getById(req.params.id, req.userInfo.email)
+            return res.send(data)
+        } catch (error) { next(error) }
+    }
+
+    async create(req, res, next) {
+        try {
+            req.body.creatorEmail = req.userInfo.email
+            let data = await commentService.create(req.body)
+            return res.status(201).send(data)
+        } catch (error) { next(error) }
+    }
+
+    async edit(req, res, next) {
+        try {
+            let data = await commentService.edit(req.params.id, req.userInfo.email, req.body)
+            return res.send(data)
+        } catch (error) { next(error) }
+    }
+
+    async delete(req, res, next) {
+        try {
+            await commentService.delete(req.params.id, req.userInfo.email)
+            return res.send("Successfully deleted")
+        } catch (error) { next(error) }
+    }
+}
+
+
