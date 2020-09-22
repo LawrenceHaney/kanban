9fcacb81547054e8163dd81cc0d251f96ef2e3ca
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-09-22 15:51:23 -0600

    started styling

--- clientsrc/src/views/Boards.vue
+++ clientsrc/src/views/Boards.vue
@@ -1,14 +1,43 @@
 <template>
-  <div class="boards">
-    WELCOME TO THE BOARDS!!!
+  <div class="boards container-fluid">
+    <div class="row justify-content-around">
+    <div class="col-3 bg-primary p-4 my-4">
+      <h1 class="card-title">Add a new board</h1>
     <form @submit.prevent="addBoard">
       <input type="text" placeholder="title" v-model="newBoard.title" required />
       <input type="text" placeholder="description" v-model="newBoard.description" />
       <button type="submit">Create Board</button>
     </form>
-    <div v-for="board in boards" :key="board.id">
-      <router-link :to="{name: 'board', params: {boardId: board.id}}">{{board.title}}</router-link>
-      <button type="button" class="btn btn-outline-primary" @click="deleteBoard(board.id)">Delete</button>
+      </div>
+      <div class="col-7 card bg-warning my-4">
+
+    <div v-for="board in boards" :key="board.id" class="row justify-content-around">
+      <card class="bg-primary p-3 col-10 my-1">
+      <router-link :to="{name: 'board', params: {boardId: board.id}}" class="btn btn-secondary justify-content-between row">
+        <div class="row">
+          <div class="col-3 bg-sucess">
+          <p>
+          {{board.creator.name}}
+          </p>
+          <img :src="board.creator.picture" alt="" class="profile">
+          
+          </div>
+          <div class="col-9">
+
+          <h4>
+          {{board.title}} 
+          </h4>
+          <h6>
+          {{board.description}}
+          </h6>
+          </div> 
+          </div>
+        <button type="button" class="btn btn-danger" @click="deleteBoard(board.id)">Delete</button>
+      </router-link>
+      
+      </card>
+      </div>
+    </div>
     </div>
   </div>
 </template>
--- clientsrc/src/views/Home.vue
+++ clientsrc/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home text-pop">
     <h1>This is your home page.</h1>
   </div>
 </template>
