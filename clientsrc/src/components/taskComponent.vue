b12b41b5681c391e283ab6f9a50c5a832d57aabc
CoreyWhitmore <cjwhit97@hotmail.com>
2020-09-22 16:55:58 -0600

    little change to list margins

--- clientsrc/src/components/taskComponent.vue
+++ clientsrc/src/components/taskComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card col-10 my-1 py-1 bg-dark text-light">
+  <div class="card col-11 my-1 py-1 bg-dark text-light">
     <div v-if="!editMode" class="row justify-content-between px-2">
       <h4 class="card-title">{{taskProp.title}} </h4>
       <div class="d-flex align-items-center">
