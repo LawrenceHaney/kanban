f3ddfb8fb6387b349117ffa10acff512d839f75d
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-09-23 11:23:58 -0600

    landing page done-ish

--- clientsrc/src/views/Home.vue
+++ clientsrc/src/views/Home.vue
@@ -1,26 +1,62 @@
 <template>
   <div class="home container-fluid">
+      <div class="row">
+        <div class="col-12 land1">
+          <div class="darken-bg row p-3">
+            <div class="col-3">
+              <img src="https://images.unsplash.com/photo-1589792924333-edbfbe7c8be7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="" class="land-img content-shadow pop fade-in">
+            </div>
+          <div class="col-8 darken-bg text-pop fade-in p-4 d-flex flex-column justify-content-around">
+            <h1 class="text-left">Kanban 看板</h1>
+              <blockquote class="blockquote text-center align-self-center">
+                <p class="mb-0"> To produce only what is needed, when it is needed and in the amount needed. </p>
+                <footer class="blockquote-footer">Taiichi Ohno</footer>
+              </blockquote>
+            <p class="align-self-end text-center">Originally implemented by Toyota in the 1940’s, Kanban fulfillment looked to provide demand based supply chains. By using cards to cue individuals of the larger needs of the team, members were empowered to collaborate efficiently. Taking inspiration from how customers interact with supermarkets, Kanban looks to provide process necessary components as required by their next step applications.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  <div class="spacer"></div>
     <div class="row">
-
-    <div class="col-12 land1">
-      <div class="darken-bg row p-3">
-      <div class="col-3">
-        <img src="https://images.unsplash.com/photo-1589792924333-edbfbe7c8be7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="" class="land-img content-shadow pop fade-in">
-      </div>
-      <div class="col-8 darken-bg text-pop fade-in p-4 d-flex flex-column justify-content-around">
-        <h1 class="text-left">Kanban 看板</h1>
-        <blockquote class="blockquote text-center align-self-center">
-          <p class="mb-0"> To produce only what is needed, when it is needed and in the amount needed. </p>
-          <footer class="blockquote-footer">Taiichi Ohno</footer>
-        </blockquote>
-        <p class="align-self-end text-center">Originally implemented by Toyota in the 1940’s, Kanban fulfillment looked to provide demand based supply chains. By using cards to cue individuals of the larger needs of the team, members were empowered to collaborate efficiently. Taking inspiration from how customers interact with supermarkets, Kanban looks to provide process necessary components as required by their next step applications.</p>
+      <div class="col-12 land1">
+        <div class="darken-bg row p-3 justify-content-between">
+          <div class="col-4 fade-in text-center">
+            <img src="https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80" alt="" class="land-img-3 pop content-shadow ">
+            <h6 class="text-pop  my-3">Efficent Orginization</h6>
+          </div>
+          <div class="col-4 fade-in text-center">
+            <img src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="" class="land-img-3 pop content-shadow ">
+            <h6 class="text-pop  my-3">Collabrtive Tracking</h6>
+          </div>
+          <div class="col-4 fade-in text-center">
+            <img src="https://images.unsplash.com/photo-1542626991-cbc4e32524cc?ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80" alt="" class="land-img-3 pop content-shadow ">
+            <h6 class="text-pop  my-3">Finishing Focused</h6>
+          </div>
+        </div>
       </div>
     </div>
+    <div class="spacer"></div>
+    <div class="row">
+      <div class="col-12 land1">
+        <div class="darken-bg row p-3">
+        <div class="col-8 darken-bg text-pop fade-in p-4 d-flex flex-column justify-content-around">
+          <h1 class="text-left">Kanban 看板</h1>
+            <blockquote class="blockquote text-center align-self-center">
+              <p class="mb-0"> To produce only what is needed, when it is needed and in the amount needed. </p>
+              <footer class="blockquote-footer">Taiichi Ohno</footer>
+            </blockquote>
+            <p class="align-self-end text-center">Originally implemented by Toyota in the 1940’s, Kanban fulfillment looked to provide demand based supply chains. By using cards to cue individuals of the larger needs of the team, members were empowered to collaborate efficiently. Taking inspiration from how customers interact with supermarkets, Kanban looks to provide process necessary components as required by their next step applications.</p>
+          </div>
+          <div class="col-3">
+            <img src="https://images.unsplash.com/photo-1589792924333-edbfbe7c8be7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt="" class="land-img content-shadow pop fade-in">
+          </div>
+        </div>
       </div>
-    <div class="row bg-light"></div>
-    <div class="row bg-light"></div>
+    </div>
   </div>
-    </div>
+</div>
+      
 </template>
 
 
