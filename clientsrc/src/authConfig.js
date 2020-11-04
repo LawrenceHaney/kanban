f3edb0ec96d2c83a770b141379291f21ac611051
Lance Haney <66646142+LawrenceHaney@users.noreply.github.com>
2020-11-04 11:30:00 -0700

    Update authConfig.js

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "corey101997.us.auth0.com";
+export const domain = "dev-lhaney.us.auth0.com";
 export const audience = "https://kanban";
-export const clientId = "gzcQjg26qEZyhmS9POrTS00tSPHct1YJ";
+export const clientId = "RVK7h7muUFfMbZXR2LQkcOX3t6eqD6aZ";
